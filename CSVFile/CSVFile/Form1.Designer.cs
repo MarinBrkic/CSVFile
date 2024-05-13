3d40b3f3b57d2db0ee100d2886f72c005bbbe17b
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-05-13 20:28:23 +0200

    Add files via upload

---
+++ CSVFile/CSVFile/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace CSVFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonUnesi = new System.Windows.Forms.Button();
+            this.textBoxPrezime = new System.Windows.Forms.TextBox();
+            this.textBoxIme = new System.Windows.Forms.TextBox();
+            this.textBoxGodina = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonUnesi
+            // 
+            this.buttonUnesi.Location = new System.Drawing.Point(83, 235);
+            this.buttonUnesi.Name = "buttonUnesi";
+            this.buttonUnesi.Size = new System.Drawing.Size(156, 38);
+            this.buttonUnesi.TabIndex = 0;
+            this.buttonUnesi.Text = "Unesi";
+            this.buttonUnesi.UseVisualStyleBackColor = true;
+            this.buttonUnesi.Click += new System.EventHandler(this.buttonUnesi_Click);
+            // 
+            // textBoxPrezime
+            // 
+            this.textBoxPrezime.Location = new System.Drawing.Point(42, 105);
+            this.textBoxPrezime.Name = "textBoxPrezime";
+            this.textBoxPrezime.Size = new System.Drawing.Size(240, 22);
+            this.textBoxPrezime.TabIndex = 1;
+            // 
+            // textBoxIme
+            // 
+            this.textBoxIme.Location = new System.Drawing.Point(42, 61);
+            this.textBoxIme.Name = "textBoxIme";
+            this.textBoxIme.Size = new System.Drawing.Size(240, 22);
+            this.textBoxIme.TabIndex = 2;
+            // 
+            // textBoxGodina
+            // 
+            this.textBoxGodina.Location = new System.Drawing.Point(42, 149);
+            this.textBoxGodina.Name = "textBoxGodina";
+            this.textBoxGodina.Size = new System.Drawing.Size(240, 22);
+            this.textBoxGodina.TabIndex = 3;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(42, 193);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(240, 22);
+            this.textBoxEmail.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 42);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ime:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Prezime:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Godina rođenja:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 316);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxGodina);
+            this.Controls.Add(this.textBoxIme);
+            this.Controls.Add(this.textBoxPrezime);
+            this.Controls.Add(this.buttonUnesi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonUnesi;
+        private System.Windows.Forms.TextBox textBoxPrezime;
+        private System.Windows.Forms.TextBox textBoxIme;
+        private System.Windows.Forms.TextBox textBoxGodina;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CSVFile/CSVFile/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace CSVFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonUnesi_Click(object sender, EventArgs e)
+        {
+            string Ime = textBoxIme.Text.Trim();
+            string Prezime = textBoxPrezime.Text.Trim();
+            string godinaRodenja = textBoxGodina.Text.Trim();
+            string Email = textBoxEmail.Text.Trim();
+
+            bool tocnoIme = !string.IsNullOrEmpty(Ime) && Ime.All(char.IsLetter);
+            bool tocnoPrezime = !string.IsNullOrEmpty(Prezime) && Prezime.All(char.IsLetter);
+
+            if (string.IsNullOrEmpty(Ime) || string.IsNullOrEmpty(Prezime) ||
+                string.IsNullOrEmpty(godinaRodenja) || string.IsNullOrEmpty(Email))
+            {
+                MessageBox.Show("Molimo vas da popunite sve traženo!", "Ne ispunjeno", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!tocnoIme)
+            {
+                MessageBox.Show("Molimo unesite ispravno ime!", "Neispravno ime",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!tocnoPrezime)
+            {
+                MessageBox.Show("Molimo unesite ispravno prezime!", "Neispravno prezime",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!IspravnaGodinaRodenja(godinaRodenja))
+            {
+                MessageBox.Show("Molimo unesite ispravnu godinu rodenja", "Neispravana godina rodenja",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!IspravanEmail(Email))
+            {
+                MessageBox.Show("Molimo unesite ispravnu e-mail adresu", "Neispravana e-mail adresa", 
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Warning);
+                return;
+            }
+
+
+            string csvPath = "User.csv";
+
+            using (StreamWriter sw = File.AppendText(csvPath))
+            {
+                sw.WriteLine($"{Ime}, {Prezime}, {godinaRodenja}, {Email}");
+            }
+
+            MessageBox.Show("Podatci su uspiješno spremljeni", "Uspijeh!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+
+            textBoxIme.Clear();
+            textBoxPrezime.Clear();
+            textBoxGodina.Clear();
+            textBoxEmail.Clear();
+        }
+
+        private bool IspravanEmail(string Email)
+        {
+            string slova = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$";
+
+            return Regex.IsMatch(Email, slova);
+        }
+
+        private bool IspravnaGodinaRodenja(string godinaRodenja)
+        {
+            if (int.TryParse(godinaRodenja, out int year))
+            {
+                if (year >= 1900 && year <= DateTime.Now.Year)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
